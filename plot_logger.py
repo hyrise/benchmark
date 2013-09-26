dd7fde92292067d58561c670d03b8ef751758e21
David Schwalb <schwalb.david@gmail.com>
2013-09-26 01:09:24 -0700

    adds mysql to settings

--- plot_logger.py
+++ plot_logger.py
@@ -9,7 +9,7 @@ fig=plt.figure()
 columns = columns_from_csv('result.csv')
 plt.bar(columns["_exp"], columns["RUNTIME_MEAN"], width=0.5,  color='r', align='center', alpha=0.4)
 plt.xticks(columns["_exp"], columns["_settingsfile"])
-plt.ylabel('CPU Cycles')
+plt.ylabel('uSecs')
 
 pp = PdfPages('foo_runtime.pdf')
 pp.savefig(fig)
