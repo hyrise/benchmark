639f6c88c366da8e9d0310dce133b1ff16a30cf0
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-10-15 15:02:00 +0200

    added benchmark module, settings class

---
+++ benchmark/__init__.py
@@ -0,0 +1,4 @@
+from benchmark import Benchmark
+from build import Build
+from settings import Settings
+from user import User
---
+++ benchmark/settings.py
@@ -0,0 +1,79 @@
+import os
+
+class Settings:
+
+    def __init__(self, name, **kwargs):
+        self._name = str(name)
+        self._dict = {
+            "PRODUCTION"           : 1,
+            "USE_PROFILER"         : 0,
+            "USE_V8"               : 0,
+            "COVERAGE_TESTING"     : 0,
+            "VAMPIR_TRACE"         : 0,
+            "PAPI_TRACE"           : 1,
+            "WITH_MYSQL"           : 1,
+            "VERBOSE_BUILD"        : 1,
+            "PERSISTENCY"          : "NONE",
+            "NVRAM_MOUNTPOINT"     : None,
+            "NVRAM_FILENAME"       : None,
+            "NVRAM_FILESIZE"       : None,
+            "NVSIMULATOR_FLUSH_NS" : None,
+            "NVSIMULATOR_READ_NS"  : None,
+            "NVSIMULATOR_WRITE_NS" : None,
+            "HYRISE_ALLOCATOR"     : None,
+            "COMPILER"             : "g++48",
+            "V8_BASE_DIRECTORY"    : None
+        }
+        for k, v in kwargs.iteritems():
+            self.__setitem__(k, v)
+
+    def __getitem__(self, key):
+        if not self._dict.has_key(key):
+            raise Exception("'%s' is not a valid setting" % k)
+        else:
+            return self._dict[key]
+
+    def __setitem__(self, key, value):
+        if not self._dict.has_key(key):
+            raise Exception("'%s' is not a valid setting" % k)
+        else:
+            self._dict[key] = value
+
+    def __str__(self):
+        return self.toString()
+
+    def getName(self):
+        return self._name
+
+    def toString(self):
+        s = "# settings.mk '" + self._name + "'"
+        for k, v in self._dict.iteritems():
+            if v != None:
+                s += "\n%s := %s" % (k, str(v))
+        return s
+
+    def writeToFile(self, filename):
+        f = open(filename, "w")
+        f.write(self.toString())
+        f.close()
+
+    def isSameAs(self, filename):
+        other = Settings.fromFile(filename)
+        for k, v in self._dict.iteritems():
+            if str(v) != str(other[k]):
+                return False
+        return True
+
+    @classmethod
+    def fromFile(cls, filename, name=None):
+        if not os.path.isfile(filename):
+            raise Exception("'%s' is not a valid file" % filename)
+        d = {}
+        n = name if name != None else os.path.basename(filename)
+        for rawline in open(filename):
+            line = "".join(rawline.split())
+            if line[0] == "#" or len(line) < 5:
+                continue
+            k, v = line.split(":=")
+            d[k] = v
+        return cls(n, **d)
