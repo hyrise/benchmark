6cd6b8e68ebf2d59ee14f506d30bb0e1cbfd6861
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-10-15 15:04:32 +0200

    added benchmark class

---
+++ benchmark/benchmark.py
@@ -0,0 +1,180 @@
+import build
+import httplib
+import os
+import queries
+import settings
+import signal
+import subprocess
+import sys
+import time
+import urllib
+import user
+
+class Benchmark:
+
+    def __init__(self, benchmarkGroupId, buildSettings, **kwargs):
+        self._id            = benchmarkGroupId
+        self._buildSettings = buildSettings
+        self._numUsers      = kwargs["numUsers"] if kwargs.has_key("numUsers") else 1
+        self._mysqlDB       = kwargs["mysqlDB"] if kwargs.has_key("mysqlDB") else "cbtr"
+        self._mysqlHost     = kwargs["mysqlHost"] if kwargs.has_key("mysqlHost") else "vm-hyrise-jenkins.eaalab.hpi.uni-potsdam.de"
+        self._mysqlPort     = kwargs["mysqlPort"] if kwargs.has_key("mysqlPort") else 3306
+        self._mysqlUser     = kwargs["mysqlUser"] if kwargs.has_key("mysqlUser") else "hyrise"
+        self._mysqlPass     = kwargs["mysqlPass"] if kwargs.has_key("mysqlPass") else "hyrise"
+        self._papi          = kwargs["papi"] if kwargs.has_key("papi") else "NO_PAPI"
+        self._prepQueries   = kwargs["prepareQueries"] if kwargs.has_key("prepareQueries") else queries.QUERIES_PREPARE
+        self._queries       = kwargs["benchmarkQueries"] if kwargs.has_key("benchmarkQueries") else queries.QUERIES_ALL
+        self._host          = kwargs["host"] if kwargs.has_key("host") else "127.0.0.1"
+        self._port          = kwargs["port"] if kwargs.has_key("port") else 5000
+        self._warmuptime    = kwargs["warmuptime"] if kwargs.has_key("warmuptime") else 0
+        self._runtime       = kwargs["runtime"] if kwargs.has_key("runtime") else 5
+        self._thinktime     = kwargs["thinktime"] if kwargs.has_key("thinktime") else 0
+        self._manual        = kwargs["manual"] if kwargs.has_key("manual") else False
+        self._dirBinary     = os.path.join(os.getcwd(), "builds/%s" % buildSettings.getName())
+        self._dirResults    = os.path.join(os.getcwd(), "results/%s/%s" % (self._id, buildSettings.getName()))
+        self._queryDict     = self._readDefaultQueryFiles()
+        self._build         = None
+        self._serverProc    = None
+        self._users         = []
+        self._connection    = None
+
+        if not os.path.isdir(self._dirResults):
+            os.makedirs(self._dirResults)
+
+    def run(self):
+        signal.signal(signal.SIGINT, self._signalHandler)
+
+        print "+------------------+"
+        print "| HYRISE benchmark |"
+        print "+------------------+\n"
+
+        if not self._manual:
+            self._buildServer()
+            self._startServer()
+            print "---\nHYRISE server running on port %s\n---" % self._port
+        else:
+            print "---\nManual mode, expecting HYRISE server running on port %s\n---" % self._port
+
+        self._runPrepareQueries()
+
+        self._createUsers()
+        sys.stdout.write("Starting %s user(s)...\r" % self._numUsers)
+        sys.stdout.flush()
+        for i in range(self._numUsers):
+            sys.stdout.write("Starting %s user(s)... %i%%      \r" % (self._numUsers, (i+1.0) / self._numUsers * 100))
+            sys.stdout.flush()
+            self._users[i].start()
+        print "Starting %s user(s)... done     " % self._numUsers
+
+        for i in range(self._warmuptime):
+            sys.stdout.write("Warming up... %i   \r" % (self._warmuptime - i))
+            sys.stdout.flush()
+            time.sleep(1)
+        print "Warming up... done     "
+
+        sys.stdout.write("Logging results for %i seconds... \r" % self._runtime)
+        sys.stdout.flush()
+        for i in range(self._numUsers):
+            self._users[i].startLogging()
+        time.sleep(self._runtime)
+        for i in range(self._numUsers):
+            self._users[i].stopLogging()
+        print "Logging results for %i seconds... done" % self._runtime
+
+        sys.stdout.write("Stopping %s user(s)...\r" % self._numUsers)
+        sys.stdout.flush()
+        for i in range(self._numUsers):
+            self._users[i].stop()
+        for i in range(self._numUsers):
+            sys.stdout.write("Stopping %s user(s)... %i%%      \r" % (self._numUsers, (i+1.0) / self._numUsers * 100))
+            sys.stdout.flush()
+            self._users[i].join()
+        print "Stopping %s user(s)... done     " % self._numUsers
+        self._stopServer()
+
+        avg, total = 0, 0
+        for i in range(self._numUsers):
+            total += self._users[i]._totalQueries
+            avg += self._users[i].getThroughput()
+        print "\nThroughput: %f transactions per second (%i total)\n================\n" % (avg, total)
+
+
+    def addQueryFile(self, queryId, filename):
+        if self._queryDict.has_key(queryId):
+            raise Exception("a query with id '%s' is already registered" % queryId)
+        else:
+            self._queryDict[queryId] = open(filename).read()
+
+    def _readDefaultQueryFiles(self):
+        cwd = os.getcwd()
+        queryDict = {}
+        for queryId, filename in queries.QUERY_FILES.iteritems():
+            queryDict[queryId] = open(os.path.join(cwd, filename)).read()
+        return queryDict
+
+    def _buildServer(self):
+        sys.stdout.write("Building server for build '%s'... " % self._buildSettings.getName())
+        sys.stdout.flush()
+        if self._build == None:
+            self._build = build.Build(settings=self._buildSettings)
+            self._build.makeAll()
+        print "done"
+
+    def _startServer(self):
+        sys.stdout.write("Starting server for build '%s'... " % self._buildSettings.getName())
+        sys.stdout.flush()
+        env = {
+            "HYRISE_DB_PATH"    : self._dirBinary,
+            "LD_LIBRARY_PATH"   : self._dirBinary+":/usr/local/lib64/",
+            "HYRISE_MYSQL_PORT" : str(self._mysqlPort),
+            "HYRISE_MYSQL_HOST" : self._mysqlHost,
+            "HYRISE_MYSQL_USER" : self._mysqlUser,
+            "HYRISE_MYSQL_PASS" : self._mysqlPass
+        }
+        server = os.path.join(self._dirBinary, "hyrise_server")
+        self._serverProc = subprocess.Popen([server, "--port=%s" % self._port], cwd=self._dirBinary, env=env, stdout=open("/dev/null"), stderr=open("/dev/null"))
+        time.sleep(1)
+        print "done"
+
+    def _runPrepareQueries(self):
+        if self._prepQueries == None or len(self._prepQueries) == 0:
+            return
+        numQueries = len(self._prepQueries)
+        connection = httplib.HTTPConnection("%s:%s" % (self._host, self._port))
+        for i, q in enumerate(self._prepQueries):
+            sys.stdout.write("Running prepare queries... %i%%      \r" % ((i+1.0) / numQueries * 100))
+            sys.stdout.flush()
+            queryString = self._queryDict[q] % {"db": self._mysqlDB}
+            data = urllib.urlencode({"query": queryString})
+            headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+            try:
+                connection.request("POST", "/", data, headers)
+                response = connection.getresponse()
+                response.status, response.reason
+                response.read()
+            except Exception:
+                print "Running prepare queries... %i%% --> Error" % ((i+1.0) / numQueries * 100)
+        print "Running prepare queries... done"
+        connection.close()
+
+    def _createUsers(self):
+        for i in range(self._numUsers):
+            self._users.append(user.User(userId=i, host=self._host, port=self._port, dirOutput=self._dirResults, queryDict=self._queryDict, queries=self._queries, thinktime=self._thinktime))
+
+    def _stopServer(self):
+        if not self._manual and self._serverProc:
+            sys.stdout.write("Stopping server... ")
+            sys.stdout.flush()
+            self._serverProc.terminate()
+            print "done."
+
+    def _signalHandler(self, signal, frame):
+        print "\n*** received SIGINT, initiating graceful shutdown"
+        for u in self._users:
+            u.stopLogging()
+            u.stop()
+        for u in self._users:
+            u.join()
+        self._stopServer()
+        exit()
+
