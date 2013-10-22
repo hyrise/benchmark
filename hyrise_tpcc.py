17ed36a410e6f02374639292285ce98274539885
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-10-22 16:48:02 +0200

    adjusted hyrise_tpcc script

--- hyrise_tpcc.py
+++ hyrise_tpcc.py
@@ -14,7 +14,7 @@ from tpcc import *
 class TPCCUser(benchmark.User):
 
     def __init__(self, userId, host, port, dirOutput, queryDict, **kwargs):
-        benchmark.User.__init__(self, userId, host, port, dirOutput, queryDict, kwargs)
+        benchmark.User.__init__(self, userId, host, port, dirOutput, queryDict, **kwargs)
         self.scaleParameters = kwargs["scaleParameters"]
         self.config = kwargs["config"]
         self.config["reset"] = False
@@ -22,7 +22,7 @@ class TPCCUser(benchmark.User):
         self.config["querylog"] = None
         self.config["print_load"] = False
         self.config["port"] = self._port
-        self.driver = HyriseDriver()
+        self.driver = drivers.hyrisedriver.HyriseDriver("")
         self.driver.loadConfig(self.config)
         self.driver.conn = self
         self.context = None
@@ -47,17 +47,18 @@ class TPCCUser(benchmark.User):
     # HyriseConnection stubs
     # ======================
     def query(self, querystr, paramlist=None, commit=False):
-        print "query"
         for k,v in paramlist.iteritems():
             if v == True:    v = 1;
             elif v == False: v = 0;
+        #print "=== fire query:", querystr
         result = self.fireQuery(querystr, paramlist, sessionContext=self.context, autocommit=commit).json()
+        #print "=== response:", result
         self.context = result.get("session_context", None)
         self.lastResult = result.get("rows", None)
         self.lastHeader = result.get("header", None)
+        #print "=== self.lastResult is now", self.lastResult
 
     def commit(self):
-        print "commit"
         if not self.context:
             raise Exception("Should not commit without running context")
         result = self.fireQuery("""{"operators": {"cm": {"type": "Commit"}}}""", sessionContext=self.context)
@@ -65,38 +66,32 @@ class TPCCUser(benchmark.User):
         return result
 
     def rollback(self):
-        print "rollback"
         if not self.context:
-            raise Exception("Should not commit without running context")
+            raise Exception("Should not rollback without running context")
         result = self.fireQuery("""{"operators": {"rb": {"type": "Rollback"}}}""", sessionContext=self.context)
         self.context = None
         return result
 
     def runningTransactions(self):
-        print "runningTransactions"
         return self._session.get("http://%s:%s/status/tx" % (self._host, self._port)).json()
 
     def fetchone(self, column=None):
-        print "fetchone"
-        if self.result:
+        if self.lastResult:
             r = self.lastResult.pop()
             return r[self.lastHeader.index(column)] if column else r
         return None
 
     def fetchone_as_dict(self):
-        print "fetchone_as_dict"
         if self.lastResult:
             return dict(zip(self.lastHeader, self.lastResult.pop()))
         return None
 
     def fetchall(self):
-        print "fetchall"
         tmp = self.lastResult
         self.lastResult = None
         return tmp
 
     def fetchall_as_dict(self):
-        print "fetchall_as_dict"
         if self.lastResult:
             r = [dict(zip(self.lastHeader, cur_res)) for cur_res in self.lastResult]
             self.lastResult = None
@@ -115,6 +110,9 @@ class TPCCBenchmark(benchmark.Benchmark):
         self.warehouses      = kwargs["warehouses"] if kwargs.has_key("warehouses") else 4
         self.driverClass     = createDriverClass("hyrise")
         self.driver          = self.driverClass(os.path.join(os.getcwd(), "pytpcc/tpcc.sql"))
+        self.driver.confirm  = False
+        #self.driver.conn     = self
+        self.driver.tables   = ["CUSTOMER", "DISTRICT", "HISTORY", "ITEM", "NEW_ORDER", "ORDER_LINE", "ORDERS", "STOCK", "WAREHOUSE"]
         self.scaleParameters = scaleparameters.makeWithScaleFactor(self.warehouses, self.scalefactor)
 
     def benchPrepare(self):
@@ -139,7 +137,8 @@ class TPCCBenchmark(benchmark.Benchmark):
         config["port"] = self._port
         self.driver.loadConfig(config)
 
-        l = loader.Loader(self.driver, self.scaleParameters, range(self.scaleParameters.starting_warehouse, self.scaleParameters.ending_warehouse+1), True)
+        #l = loader.Loader(self.driver, self.scaleParameters, range(self.scaleParameters.starting_warehouse, self.scaleParameters.ending_warehouse+1), True)
+        self.driver.executeStart()
         #l.execute()
 
         self._userArgs = {
@@ -161,10 +160,22 @@ if __name__ == "__main__":
                          help='Disable loading the data')
     aparser.add_argument('--no-execute', action='store_true',
                          help='Disable executing the workload')
+    aparser.add_argument('--port', default=5001, type=int, metavar="P",
+                         help='Port on which HYRISE should be run')
+    aparser.add_argument('--manual', action='store_true',
+                         help='Do not build and start a HYRISE instance (note: a HYRISE server must be running on the specified port)')
     args = vars(aparser.parse_args())
 
     s1 = benchmark.Settings("none", PERSISTENCY="NONE")
 
-    b1 = TPCCBenchmark("testrun", s1, port=5001, warmuptime=10, runtime=args["duration"], numUsers=args["clients"], warehouses=args["warehouses"], benchmarkQueries=["q13insert"], prepareQueries=[])
+    b1 = TPCCBenchmark("testrun", s1,
+                       port=args["port"],
+                       manual=args["manual"],
+                       warmuptime=10,
+                       runtime=args["duration"],
+                       numUsers=args["clients"],
+                       warehouses=args["warehouses"],
+                       benchmarkQueries=[],
+                       prepareQueries=[])
 
     b1.run()
