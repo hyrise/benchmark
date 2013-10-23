a4dfc6e3f2136053c053d632aae4a3f5af593c00
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-10-23 17:18:08 +0200

    Settings now allows for building with old build system via oldMode=True in constructor

--- benchmark/settings.py
+++ benchmark/settings.py
@@ -2,24 +2,43 @@ import os
 
 class Settings:
 
-    def __init__(self, name, **kwargs):
+    def __init__(self, name, oldMode=False, **kwargs):
         self._name = str(name)
-        self._dict = {
-            "BLD"                  : "release",
-            "WITH_PROFILER"        : None,
-            "WITH_V8"              : None,
-            "WITH_PAPI"            : None,
-            "WITH_MYSQL"           : None,
-            "VERBOSE_BUILD"        : None,
-            "PERSISTENCY"          : "NONE",
-            "NVRAM_MOUNTPOINT"     : None,
-            "NVRAM_FILENAME"       : None,
-            "NVRAM_FILESIZE"       : None,
-            "NVSIMULATOR_FLUSH_NS" : None,
-            "NVSIMULATOR_READ_NS"  : None,
-            "NVSIMULATOR_WRITE_NS" : None,
-            "COMPILER"             : None,
-        }
+        self._oldMode = oldMode
+        if self._oldMode:
+            self._dict = {
+                "PRODUCTION"           : 1,
+                "WITH_PROFILER"        : None,
+                "WITH_V8"              : None,
+                "WITH_PAPI"            : None,
+                "WITH_MYSQL"           : None,
+                "VERBOSE_BUILD"        : None,
+                "PERSISTENCY"          : "NONE",
+                "NVRAM_MOUNTPOINT"     : None,
+                "NVRAM_FILENAME"       : None,
+                "NVRAM_FILESIZE"       : None,
+                "NVSIMULATOR_FLUSH_NS" : None,
+                "NVSIMULATOR_READ_NS"  : None,
+                "NVSIMULATOR_WRITE_NS" : None,
+                "COMPILER"             : None,
+            }
+        else:
+            self._dict = {
+                "BLD"                  : "release",
+                "WITH_PROFILER"        : None,
+                "WITH_V8"              : None,
+                "WITH_PAPI"            : None,
+                "WITH_MYSQL"           : None,
+                "VERBOSE_BUILD"        : None,
+                "PERSISTENCY"          : "NONE",
+                "NVRAM_MOUNTPOINT"     : None,
+                "NVRAM_FILENAME"       : None,
+                "NVRAM_FILESIZE"       : None,
+                "NVSIMULATOR_FLUSH_NS" : None,
+                "NVSIMULATOR_READ_NS"  : None,
+                "NVSIMULATOR_WRITE_NS" : None,
+                "COMPILER"             : None,
+            }
         for k, v in kwargs.iteritems():
             self.__setitem__(k, v)
 
@@ -41,6 +60,9 @@ class Settings:
     def getName(self):
         return self._name
 
+    def oldMode(self):
+        return self._oldMode
+
     def toString(self):
         s = "# settings.mk '" + self._name + "'"
         for k, v in self._dict.iteritems():
@@ -54,14 +76,17 @@ class Settings:
         f.close()
 
     def isSameAs(self, filename):
-        other = Settings.fromFile(filename)
-        for k, v in self._dict.iteritems():
-            if str(v) != str(other[k]):
-                return False
+        try:
+            other = Settings.fromFile(filename, oldMode=self._oldMode)
+            for k, v in self._dict.iteritems():
+                if str(v) != str(other[k]):
+                    return False
+        except Exception:
+            return False
         return True
 
     @classmethod
-    def fromFile(cls, filename, name=None):
+    def fromFile(cls, filename, name=None, oldMode=False):
         if not os.path.isfile(filename):
             raise Exception("'%s' is not a valid file" % filename)
         d = {}
@@ -72,4 +97,4 @@ class Settings:
                 continue
             k, v = line.split(":=")
             d[k] = v
-        return cls(n, **d)
+        return cls(n, oldMode=oldMode, **d)
