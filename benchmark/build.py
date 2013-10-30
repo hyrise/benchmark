0e345c4d0dfb80807b4b36f2520cb16ad00942c9
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-10-30 17:03:26 +0100

    fixed issue with Build class and incremental builds with new build system, rebuilding is now no longer required

--- benchmark/build.py
+++ benchmark/build.py
@@ -10,14 +10,16 @@ class Build:
     def __init__(self, settings):
         if not isinstance(settings, Settings):
             raise Exception("You must provide either a valid settings file name or Settings object instance")
-        self._settings     = settings
-        self._dirSource    = os.path.join(os.getcwd(), "hyrise")
-        self._dirBuild     = os.path.join(self._dirSource, "build")
-        self._dirBuildBak  = os.path.join(self._dirSource, "build.bak")
-        self._dirResult    = os.path.join(os.getcwd(), "builds/" + settings.getName())
-        self._dirLog       = os.path.join(os.getcwd(), "logs/" + settings.getName())
-        self._logfile      = os.path.join(self._dirLog, "build_log.txt")
-        self._settingsfile = os.path.join(self._dirResult, "settings.mk")
+        self._settings           = settings
+        self._dirSource          = os.path.join(os.getcwd(), "hyrise")
+        self._dirBuild           = os.path.join(self._dirSource, "build")
+        self._dirBuildBak        = os.path.join(self._dirSource, "build.bak")
+        self._dirHiddenBuild     = os.path.join(self._dirSource, ".build")
+        self._dirHiddenBuildBak  = os.path.join(self._dirSource, ".build.bak")
+        self._dirResult          = os.path.join(os.getcwd(), "builds/" + settings.getName())
+        self._dirLog             = os.path.join(os.getcwd(), "logs/" + settings.getName())
+        self._logfile            = os.path.join(self._dirLog, "build_log.txt")
+        self._settingsfile       = os.path.join(self._dirResult, "settings.mk")
         self._prepare()
 
     def makeAll(self):
@@ -33,8 +35,11 @@ class Build:
 
     def makeClean(self):
         self.link()
-        process = subprocess.Popen("make clean", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd=self._dirSource)
-        (stdout, stderr) = process.communicate()
+        try:
+            process = subprocess.Popen("make clean", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd=self._dirSource)
+            (stdout, stderr) = process.communicate()
+        except Exception:
+            pass
         self.unlink()
 
     def link(self):
@@ -43,7 +48,12 @@ class Build:
             os.rename(self._dirBuild, self._dirBuildBak)
         elif os.path.islink(self._dirBuild):
             os.remove(self._dirBuild)
+        if os.path.isdir(self._dirHiddenBuild) and not os.path.islink(self._dirHiddenBuild):
+            os.rename(self._dirHiddenBuild, self._dirHiddenBuildBak)
+        elif os.path.islink(self._dirHiddenBuild):
+            os.remove(self._dirHiddenBuild)
         os.symlink(self._dirResult, self._dirBuild)
+        os.symlink(os.path.join(self._dirResult, ".build"), self._dirHiddenBuild)
         if os.path.isfile(settingsFile):
             os.rename(settingsFile, settingsFile+".bak")
         shutil.copy2(self._settingsfile, settingsFile)
@@ -52,10 +62,14 @@ class Build:
         settingsFile = os.path.join(self._dirSource, "settings.mk")
         if os.path.islink(self._dirBuild):
             os.remove(self._dirBuild)
+        if os.path.islink(self._dirHiddenBuild):
+            os.remove(self._dirHiddenBuild)
         if os.path.isdir(self._dirBuildBak):
             os.rename(self._dirBuildBak, self._dirBuild)
+        if os.path.isdir(self._dirHiddenBuildBak):
+            os.rename(self._dirHiddenBuildBak, self._dirHiddenBuild)
+        os.remove(settingsFile)
         if os.path.isfile(settingsFile+".bak"):
-            os.remove(settingsFile)
             os.rename(settingsFile+".bak", settingsFile)
 
     def _prepare(self):
@@ -74,5 +88,7 @@ class Build:
         if not os.path.exists(self._settingsfile) or not self._settings.isSameAs(self._settingsfile):
             self._settings.writeToFile(self._settingsfile)
 
+        if not os.path.isdir(os.path.join(self._dirResult, ".build")):
+            os.mkdir(os.path.join(self._dirResult, ".build"))
         if not os.path.exists(self._dirLog):
             os.makedirs(self._dirLog)
