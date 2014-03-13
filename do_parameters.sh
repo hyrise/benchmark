78e8bb7b5a31fb2cc3d920c5d6e8201a1d4c591d
David Schwalb <schwalb.david@gmail.com>
2014-03-13 16:21:14 +0100

    use 20W tables as default

--- do_parameters.sh
+++ do_parameters.sh
@@ -4,7 +4,7 @@
 CLIENTS="1000"
 PORT="5222"
 QUERYFILE="/mnt/amida_02/tpcc/queries_wid/20W_neworder_10M.txt"
-TABLEDIR="/mnt/amida_02/tpcc/0W-tables"
+TABLEDIR="/mnt/amida_02/tpcc/20W-tables"
 DURATION="60"
 VERBOSE="0"
 PARAMETER="--ab=$QUERYFILE --clients=$CLIENTS --threads=20 --abCore=22 --port=$PORT --tabledir=$TABLEDIR --duration=$DURATION --verbose=$VERBOSE --stdout --stderr"
