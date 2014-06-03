f3e81175e97897643aceb00022808a69c126feb7
David Schwalb <schwalb.david@gmail.com>
2014-06-03 23:13:58 +0200

    updated tpcc clients experiment

--- exp_tpcc_clients.py
+++ exp_tpcc_clients.py
@@ -1,23 +1,26 @@
 from tpcc_parameters import *
 import shutil
 
+
 groupId = "tpcc_clients_tmp"
-num_clients = args["clients"]
-minClients = args["clients_min"]
-maxClients = args["clients_max"]
-stepClients = args["clients_step"]
 
+minClients = 20
+maxClients = 301
+stepClients = 20
+runs = 5
 
-if args["clients"] > 0:
-    minClients = args["clients"]
-    maxClients = args["clients"]
+clients = [x for x in xrange(minClients, maxClients+1, stepClients)]
+clients.append(10)
+clients.append(5)
+clients.append(1)
 
-for num_clients in xrange(minClients, maxClients+1, stepClients):
-
-    parameters = {"numUsers":num_clients}
-    kwargs["numUsers"] = num_clients
-    kwargs["hyriseDBPath"] = "/mnt/fusion/david/hyrise_persistency/"
-
-    create_benchmark_none("None", groupId, parameters, kwargs).run()
-    create_benchmark_logger("Logger-30ms", groupId, parameters, kwargs, windowsize_ms=30000, checkpoint_interval_ms=0).run()
-    create_benchmark_nvram("NVRAM", groupId, parameters, kwargs).run()
+for num_clients in clients:
+    for run in range(runs):
+        parameters = {"numUsers":num_clients, "run":run}
+        kwargs["numUsers"] = num_clients
+        kwargs["hyriseDBPath"] = "/mnt/fusion/david/hyrise_persistency/"
+    
+        create_benchmark_none("None", groupId, parameters, kwargs).run()
+        create_benchmark_logger("Logger-10ms", groupId, parameters, kwargs, windowsize_ms=10, checkpoint_interval_ms=0).run()
+        # create_benchmark_logger("Logger-5ms", groupId, parameters, kwargs, windowsize_ms=5, checkpoint_interval_ms=0).run()
+        create_benchmark_nvram("NVRAM", groupId, parameters, kwargs).run()
