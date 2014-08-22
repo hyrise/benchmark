6444a9f94249c88fee361ae236fed1beb9bd8fb0
Markus Dreseler <markus@dreseler.de>
2014-08-22 14:12:04 +0200

    Visualizer: Events, multi-threading for server

--- visualizer/run.py
+++ visualizer/run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import SimpleHTTPServer
+import BaseHTTPServer
 import SocketServer
 import json
 import os
@@ -10,7 +11,10 @@ from functools import partial
 import os.path, time
 
 
-AB_LOGS = {'NVRAM': '../results/recovery_demo/{}/NVRAM/ab.log'}
+AB_LOGS = {
+    'NVRAM': '../results/recovery_demo/{}/NVRAM/',
+    # 'Logger': '../results/recovery_demo/{}/Logger/',
+}
 
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -31,32 +35,58 @@ class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             self.send_header("Content-type:", "application/json")
             self.end_headers()
 
-            fname = AB_LOGS[self.path.replace('/', '').replace('.json', '')]
+            resultdir = AB_LOGS[self.path.replace('/', '').replace('.json', '')]
+            fname = resultdir + "ab.log"
             if not os.path.isfile(fname):
                 print "ab log not found " + fname
                 return
 
-            df = pd.read_csv(fname, sep="\t")
+            df = pd.read_csv(fname, sep="\t", engine='c', usecols=['seconds', 'txname'], dtype={
+                'seconds': np.uint64,
+                'txname': object,
+            })
             df = df[df.txname == "TPCC-NewOrder"]
 
             if math.isnan(df.seconds.max()):
                 print "no valid data found " + fname
                 return
 
-            df.seconds -= int(df.seconds.min())
+            start_timestamp = int(df.seconds.min())
+            df.seconds -= start_timestamp
             df.seconds /= 1e6
             df.seconds = np.round(df.seconds, 1)
-            df = df.status.groupby(df.seconds).agg('count')
+            df = df.txname.groupby(df.seconds).agg('count')
 
             df = df.head(len(df.index)-10)
+
+            self.wfile.write('{"data":')                
             df.to_json(path_or_buf=self.wfile) # leave out last rows because they might be incomplete
+
+            self.wfile.write(', "events":')                
+            eventfile = resultdir + "/events.json"
+            if os.path.isfile(eventfile):
+                with open(eventfile) as f:
+                    events = json.load(f)
+                events_normalized = {}
+                for k, v in events.iteritems():
+                    print int(k.replace('.',''))
+                    print start_timestamp
+                    events_normalized[(int(k.replace('.',''))-start_timestamp)/1e6] = v
+                self.wfile.write(json.dumps(events_normalized))
+            else:
+                self.wfile.write('[]')                
+            self.wfile.write('}')                
 
             print str(time.time() - start_time) + " elapsed"
             return
         print "route not found " + self.path
 
+class ThreadingServer(SocketServer.ThreadingMixIn,
+                   BaseHTTPServer.HTTPServer):
+    pass
+
 Handler = MyRequestHandler
-SocketServer.TCPServer.allow_reuse_address = True
-server = SocketServer.TCPServer(('0.0.0.0', 8080), Handler)
+ThreadingServer.allow_reuse_address = True
+server = ThreadingServer(('0.0.0.0', 8080), Handler)
 
 server.serve_forever()
